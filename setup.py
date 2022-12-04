944f49846c630d6f2b92012aa9bfc996023d167e
kushalpatil122 <kushal.patil121@gmail.com>
2022-12-04 09:03:10 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="Sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="kushal.patil121@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements(),
+)
